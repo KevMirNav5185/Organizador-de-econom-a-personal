a696d785920f44c7ceec2c711f8cd2ef3b8801e3
KevMirNav5185 <88916232+KevMirNav5185@users.noreply.github.com>
2021-09-03 06:48:47 -0500

    Implementación de funciones e indicaciones PEP 8
    Se agregaron dos funciones:
    - Calcula el total de egresos al sumar todas las categorías de gasto
    - Calcula el balance final al restarle el total de egresos
    Se actualizó la descripción del programa, se agregaron separaciones entre la zona de funciones y la del código principal, y se cortaron las líneas de código que excedían los 749caracteres.

--- program.py
+++ program.py
@@ -1,52 +1,71 @@
-# -*- coding: utf-8 -*-
 """
-Created on Fri Aug 27 22:39:56 2021
-
-@author: Miranda Navarrete 1
-"""
-
-"""
+Organizador de economía personal.
 El programa es un desglosador de ingresos mensuales en categorías de egresos.
-E0 (Recibe ingreso mensual)
-E1 (Entrega desglose del ingreso por categorías y un balance positivo, negativo o nulo)
+Recibe un ingreso mensual y las cantidades de dinero que el usuario gasta 
+por ciertas categorías. Al final entrega un balance y ya sea un desglose
+completo por categoría de egreso, o solo las categorías de mayor
+y menor gasto -en proceso-.
 """
-print("Te damos la bienvenida al sistema Tu Dinero Tu Bienestar (TDTB)\n")
 
-balance=float(input("¿Cuánto dinero recibiste? -Ingreso mensual\n"))
+"""-----------------------Comienza-Funciones--------------------------------"""
 
-print("Tu ingreso mensual es de: ", balance, " MXN \n")
+def egresos_totales(egresos):
+    """
+    Recibe: estado inicial de egresos (egresos=0)
+    Se le suman todas las categorías de costo
+    Devuelve: egresos totales
+    """
+    egresos=egresos + costo_agua
+    egresos=egresos + costo_electricidad
+    egresos=egresos + costo_telefono_cable
+    egresos=egresos + costo_gas
+    egresos=egresos + costo_transporte
+    egresos=egresos + costo_extra
+    return egresos
 
-costo_agua=float(input("¿Cuánto debes pagar por agua durante el mes? \n"))
+def calculo_balance(balance,egresos):
+    """
+    Recibe: ingreso mensual del usuario (balance) y estado inicial de 
+    egresos (egresos=0)
+    Se le resta al balance el total de egresos (egresos) calculado en la 
+    funcion de egresos_totales
+    Devuelve: balance reducido
+    """
+    return balance - egresos_totales(egresos)
 
-balance = balance-costo_agua
+"""----------------------Comienza-Código-Principal--------------------------"""
 
-costo_electricidad=float(input("¿Cuánto debes pagar por electricidad durante el mes? \n"))
+egresos=0
 
-balance = balance-costo_electricidad
+print("Te damos la bienvenida al sistema Tu Dinero Tu Bienestar (TDTB)")
 
-costo_tel_cable=float(input("¿Cuánto debes pagar por teléfono y cable durante el mes? \n"))
+balance = float(input("¿Cuánto dinero recibiste? -Ingreso mensual\n"))
 
-balance = balance-costo_tel_cable
+print("\nTu ingreso mensual es de: ", balance, " MXN \n")
 
-costo_gas=float(input("¿Cuánto debes pagar por gas durante el mes? \n"))
+costo_agua = float(input("¿Cuánto debes pagar por agua durante el mes? \n"))
 
-balance = balance-costo_gas
+costo_electricidad = float(input("¿Cuánto debes pagar por electricidad durante\
+ el mes? \n"))
 
-costo_transporte=float(input("¿Cuánto gastas en transporte (tarifa de camión o gasto en gasolina/servicio de transporte) durante el mes? \n"))
+costo_telefono_cable = float(input("¿Cuánto debes pagar por teléfono y cable\
+ durante el mes? \n"))
 
-balance = balance-costo_transporte
+costo_gas = float(input("¿Cuánto debes pagar por gas durante el mes? \n"))
 
-costo_extra=float(input("¿Cuánto debes por algún gasto extra \n"))
+costo_transporte = float(input("¿Cuánto gastas en transporte (tarifa de camión\
+ o gasto en gasolina/servicio de transporte) durante el mes? \n"))
 
-balance = balance-costo_extra
+costo_extra = float(input("¿Cuánto debes por algún gasto extra \n"))
 
-print("Tu balance para este mes es de: ", balance, " MXN\n" )
+
+print("\nTu balance para este mes es de: ", calculo_balance(balance,egresos), " MXN\n")
 
 print("Pagas por agua: ", costo_agua, " MXN\n")
 
 print("Pagas por electricidad: ", costo_electricidad, " MXN\n")
 
-print("Pagas por teléfono y cable: ", costo_tel_cable, " MXN\n")
+print("Pagas por teléfono y cable: ", costo_telefono_cable, " MXN\n")
 
 print("Pagas por gas: ", costo_gas, " MXN\n")
 
