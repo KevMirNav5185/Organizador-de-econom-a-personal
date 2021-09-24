a666ad053054082281d9b8c57afa7c155c87e1c9
KevMirNav5185 <88916232+KevMirNav5185@users.noreply.github.com>
2021-09-24 06:26:11 -0500

    Implementación: cálculo de tiempo para ahorro
    Se implementó como tercera función general del programa una que calcula el tiempo necesario en meses para alcanzar una meta de ahorro, dados ciertos parámetros y utilizando un ciclo while. Se creó una nueva función dentro del apartado de funciones, se agregó la recopilación de parámetros y muestra de resultados dentro del código principal, y se ajusto el programa en general.

--- program.py
+++ program.py
@@ -35,8 +35,10 @@ def calculo_balance(balance, egresos):
     funcion de egresos_totales
     Devuelve: balance reducido
     """
-    return balance - egresos_totales(egresos, costo_agua, costo_electricidad,\
+    balance = balance - egresos_totales(egresos, costo_agua, costo_electricidad,\
     costo_telefono_cable, costo_gas, costo_transporte, costo_extra)
+    
+    return balance
 
 
 def identifica_categoria(valor_categoria, costo_agua, costo_electricidad,\
@@ -60,6 +62,39 @@ def identifica_categoria(valor_categoria, costo_agua, costo_electricidad,\
     return " MXN por transporte"
    else:
     return "MXN por extra"
+
+
+def tiempo_para_ahorro (dinero_acumulado, ahorro, balance,egresos,\
+    meta_ahorro, porcentaje_ahorro, incremento_ingresos,\
+    periodo_incremento_ingresos, incremento_egresos,\
+    periodo_incremento_egresos,meses):
+    
+    """
+    Recibe: una variable acumuladora (dinero acumulado), una variable
+    ajustable para que el cálculo del ahorro se simplifique (ahorro),
+    el balance, los egresos totales, una meta de ahorro, cuánto y 
+    cada cuánto aumentaran los ingresos y egresos, y un contador
+    que es lo que se requiere calcular (meses).
+    La función calcula los meses necesarios para ahorrar cierta cantidad
+    de dinero utilizando un ciclo while y dados ciertos parámetros
+    (Explicación más detallada a un lado de los procesos)
+    Regresa: Tiempo en meses.
+    """
+    
+    while (dinero_acumulado<meta_ahorro): #Corre la operación mientras no se cumpla la meta
+    
+        ahorro=calculo_balance(balance,egresos) #El balance del mes es la base del ahorro mensual
+        ahorro=ahorro*porcentaje_ahorro #Se calcula el porcentaje ahorrado del balance
+        dinero_acumulado=dinero_acumulado + ahorro #Se acumula esta cantidad
+        
+        meses=meses+1 #Se va avanzando en el tiempo mes por mes
+        
+        if meses==periodo_incremento_ingresos: #Haz el ajuste de ingresos cuando se cumpla el perioddo
+            balance=balance + (balance*incremento_ingresos)
+        if meses==periodo_incremento_egresos:  #Haz el ajuste de egresos cuando se cumpla el perioddo
+            egresos=egresos + (egresos*incremento_egresos)
+            
+    return meses #Regresa el tiempo requerido
 
 
 """----------------------Comienza-Código-Principal--------------------------"""
@@ -91,6 +126,7 @@ costo_extra = float(input("¿Cuánto debes por algún gasto extra \n"))
 print("¿Qué necesitas?\n")
 print("1. Desglose completo de egresos y balance final\n")
 print("2. Categoría de egreso mayor y menor, con balance final\n")
+print("3. Tiempo necesario para ahorrar una cantidad\n")
 
 
 """------------------Comienzan--menú--y--operaciones-----------------------"""
@@ -125,5 +161,44 @@ elif opcion == 2:
   costo_agua,costo_electricidad, costo_telefono_cable, costo_gas,\
   costo_transporte, costo_extra))
 
+elif opcion==3:
+    meses=0 #contador
+    ahorro=0.0 #variable de cambio que se ajusta
+    dinero_acumulado=0.0  #acumulador
+    
+    #Comienza recopilación de parámetros
+    meta_ahorro= float(input("- Meta de ahorro (¿Cuánto se quiere ahorrar?): "))
+    porcentaje_ahorro= float(input("- Porcentaje del balance a ahorrar mensualmente (Formato: 0.2=20%): "))
+    
+    print("\nAhora establece cuánto y cada cuántos meses aumentaran tus ingresos aproximadamente")
+    incremento_ingresos= float(input("- Porcentaje de incremento de ingresos (Formato: 0.2=20%): "))
+    periodo_incremento_ingresos=int(input("- Periodo de incremento en meses: "))
+    
+    print("\nAhora establece cuánto y cada cuántos meses aumentaran tus egresos aproximadamente")
+    incremento_egresos=float(input("- Porcentaje de incremento de egresos (Formato: 0.2=20%): "))
+    periodo_incremento_egresos=int(input("- Periodo de incremento en meses: "))
+    
+    #Muestra de resultados
+    print("\nPara ahorrar ", meta_ahorro, " MXN bajo los siguientes parámetros: ")
+    print("\nIngresos actuales: ", balance, " MXN")
+    print("Egresos actuales: ", egresos_totales(egresos, costo_agua,\
+    costo_electricidad, costo_telefono_cable, costo_gas, costo_transporte,\
+    costo_extra), " MXN")
+    print("Ahorrando el ", porcentaje_ahorro, " del balance")
+    print("Los ingresos aumentan a razón de ",incremento_ingresos," cada ",\
+    periodo_incremento_ingresos," mes/es")
+    print("Los egresos aumentan a razón de ",incremento_egresos," cada ",\
+    periodo_incremento_egresos," mes/es")
+    print("\nSe requiere/n ", tiempo_para_ahorro (dinero_acumulado, ahorro,\
+    balance,egresos, meta_ahorro, porcentaje_ahorro, incremento_ingresos,\
+    periodo_incremento_ingresos, incremento_egresos,\
+    periodo_incremento_egresos,meses), " mes/es")
+    
+    """ Todavía falta agregar los procesos de comprobación de los valores
+    ingresados para que indique cuando haya error y los cambie. Sobre esto,
+    tengo unas dudas que quiero aclarar primero antes de agregar los procesos.
+    Ya agendé una asesoría con el profe el lunes 27 de septiembre.
+    Para el siguiente avance, ya estarán los procesos de comprobación """
+    
 else:
   print("Elija una opción válida")
