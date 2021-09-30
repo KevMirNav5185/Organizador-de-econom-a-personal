2d37ba1c63cc56726d1fdc395db1f616a86c0861
KevMirNav5185 <88916232+KevMirNav5185@users.noreply.github.com>
2021-09-30 05:48:39 -0500

    Ajustes y comprobación
    Se ajustó la función "calculo_balance" y el valor de la variable "egresos" para que la función solo trabajé con los parámetros que se le dan.  Se agregaron ciclos while para comprobar la recopilación de datos principales (balance y categorías de costos) y para comprobar los datos en la opción 3 del menú.

--- program.py
+++ program.py
@@ -15,8 +15,8 @@ def egresos_totales(egresos, costo_agua, costo_electricidad, costo_telefono_cabl
     """
     Recibe: estado inicial de egresos (egresos=0) y cantidades guardadas
     en cada categoría de costos.
-    Se le suman todas las categorías de costo
-    Devuelve: egresos totales
+    Se acumulan en egresos todas las categorías de costos
+    Devuelve: egresos totales acumulados en la variable egresos
     """
     egresos = costo_agua
     egresos = egresos + costo_electricidad
@@ -29,14 +29,11 @@ def egresos_totales(egresos, costo_agua, costo_electricidad, costo_telefono_cabl
 
 def calculo_balance(balance, egresos):
     """
-    Recibe: ingreso mensual del usuario (balance) y estado inicial de 
-    egresos (egresos=0)
-    Se le resta al balance el total de egresos (egresos) calculado en la 
-    funcion de egresos_totales
+    Recibe: ingreso mensual del usuario (balance) y los egresos acumulados
+    Se le resta al balance el total de egresos
     Devuelve: balance reducido
     """
-    balance = balance - egresos_totales(egresos, costo_agua, costo_electricidad,\
-    costo_telefono_cable, costo_gas, costo_transporte, costo_extra)
+    balance = balance - egresos
     
     return balance
 
@@ -64,10 +61,10 @@ def identifica_categoria(valor_categoria, costo_agua, costo_electricidad,\
     return "MXN por extra"
 
 
-def tiempo_para_ahorro (dinero_acumulado, ahorro, balance,egresos,\
-    meta_ahorro, porcentaje_ahorro, incremento_ingresos,\
-    periodo_incremento_ingresos, incremento_egresos,\
-    periodo_incremento_egresos,meses):
+def tiempo_para_ahorro (dinero_acumulado, ahorro, balance,egresos,meta_ahorro,
+                        porcentaje_ahorro, incremento_ingresos,
+                        periodo_incremento_ingresos,incremento_egresos,
+                        periodo_incremento_egresos,meses):
     
     """
     Recibe: una variable acumuladora (dinero acumulado), una variable
@@ -81,47 +78,72 @@ def tiempo_para_ahorro (dinero_acumulado, ahorro, balance,egresos,\
     Regresa: Tiempo en meses.
     """
     
-    while (dinero_acumulado<meta_ahorro): #Corre la operación mientras no se cumpla la meta
+    while (dinero_acumulado<meta_ahorro): #Corre la operación mientras
+                                          #no se cumpla la meta
     
-        ahorro=calculo_balance(balance,egresos) #El balance del mes es la base del ahorro mensual
-        ahorro=ahorro*porcentaje_ahorro #Se calcula el porcentaje ahorrado del balance
-        dinero_acumulado=dinero_acumulado + ahorro #Se acumula esta cantidad
+        ahorro=calculo_balance(balance,egresos) #El balance del mes es la 
+                                                #base del ahorro mensual
+        ahorro=ahorro*porcentaje_ahorro #Se calcula el porcentaje ahorrado
+                                        #del balance
+        dinero_acumulado=dinero_acumulado + ahorro #Se va acumulando el ahorro
         
         meses=meses+1 #Se va avanzando en el tiempo mes por mes
         
-        if meses==periodo_incremento_ingresos: #Haz el ajuste de ingresos cuando se cumpla el perioddo
+        if meses==periodo_incremento_ingresos: #Haz el ajuste de ingresos
+                                               #cuando se cumpla el periodo
             balance=balance + (balance*incremento_ingresos)
-        if meses==periodo_incremento_egresos:  #Haz el ajuste de egresos cuando se cumpla el perioddo
+        if meses==periodo_incremento_egresos:  #Haz el ajuste de egresos cuando
+                                               #se cumpla el periodo
             egresos=egresos + (egresos*incremento_egresos)
             
     return meses #Regresa el tiempo requerido
 
 
 """----------------------Comienza-Código-Principal--------------------------"""
-egresos = 0
-
 print("Te damos la bienvenida al sistema Tu Dinero Tu Bienestar (TDTB)")
 
 """-------------Recopilación--de--datos---------------"""
 
 balance = float(input("¿Cuánto dinero recibiste? -Ingreso mensual\n"))
+#Comprobación
+while balance<0:
+        balance=float(input("Error. Ingrese un valor positivo: "))
 
 print("\nTu ingreso mensual es de: ", balance, " MXN \n")
 
 costo_agua = float(input("¿Cuánto debes pagar por agua durante el mes? \n"))
+#Comprobación
+while costo_agua<0:
+    costo_agua=float(input("Error. Ingrese un valor positivo: "))
 
 costo_electricidad = float(input("¿Cuánto debes pagar por electricidad durante\
  el mes? \n"))
+#Comprobación
+while costo_electricidad<0:
+    costo_electricidad=float(input("Error. Ingrese un valor positivo: "))
 
 costo_telefono_cable = float(input("¿Cuánto debes pagar por teléfono y cable\
  durante el mes? \n"))
+#Comprobación
+while costo_telefono_cable<0:
+    costo_telefono_cable=float(input("Error. Ingrese un valor positivo: "))
 
 costo_gas = float(input("¿Cuánto debes pagar por gas durante el mes? \n"))
+#Comprobación
+while costo_gas<0:
+    costo_gas=float(input("Error. Ingrese un valor positivo: "))
+
 
 costo_transporte = float(input("¿Cuánto gastas en transporte (tarifa de camión\
  o gasto en gasolina/servicio de transporte) durante el mes? \n"))
+#Comprobación
+while costo_transporte<0:
+    costo_transporte=float(input("Error. Ingrese un valor positivo: "))
 
 costo_extra = float(input("¿Cuánto debes por algún gasto extra \n"))
+#Comprobación
+while costo_extra<0:
+    costo_extra=float(input("Error. Ingrese un valor positivo: "))
 
 print("¿Qué necesitas?\n")
 print("1. Desglose completo de egresos y balance final\n")
@@ -130,6 +152,11 @@ print("3. Tiempo necesario para ahorrar una cantidad\n")
 
 
 """------------------Comienzan--menú--y--operaciones-----------------------"""
+#Cálculo de los egresos totales
+egresos=0
+egresos=egresos_totales(egresos, costo_agua,costo_electricidad,
+                        costo_telefono_cable,costo_gas, costo_transporte,
+                        costo_extra)
 
 opcion = int(input("Selecciona una función: "))
 
@@ -166,24 +193,45 @@ elif opcion==3:
     ahorro=0.0 #variable de cambio que se ajusta
     dinero_acumulado=0.0  #acumulador
     
-    #Comienza recopilación de parámetros
+    """---------------Comienza recopilación de parámetros-------------------"""
+    
     meta_ahorro= float(input("- Meta de ahorro (¿Cuánto se quiere ahorrar?): "))
+    #Comprobación
+    while meta_ahorro<0:
+        meta_ahorro=float(input("Error. Ingrese un valor positivo: "))
+
     porcentaje_ahorro= float(input("- Porcentaje del balance a ahorrar mensualmente (Formato: 0.2=20%): "))
+    #Comprobación
+    while porcentaje_ahorro<0 or porcentaje_ahorro>1:
+        porcentaje_ahorro=float(input("Error. Ingrese un valor decimal entre 0 y 1: "))
     
     print("\nAhora establece cuánto y cada cuántos meses aumentaran tus ingresos aproximadamente")
     incremento_ingresos= float(input("- Porcentaje de incremento de ingresos (Formato: 0.2=20%): "))
+    #Comprobación
+    while incremento_ingresos<0 or incremento_ingresos>1: 
+        incremento_ingresos=float(input("Error. Ingrese un valor decimal entre 0 y 1: "))
+    
     periodo_incremento_ingresos=int(input("- Periodo de incremento en meses: "))
+    #Comprobación
+    while periodo_incremento_ingresos<0:
+        periodo_incremento_ingresos=int(input("Error. Ingrese un valor positivo: "))
     
     print("\nAhora establece cuánto y cada cuántos meses aumentaran tus egresos aproximadamente")
     incremento_egresos=float(input("- Porcentaje de incremento de egresos (Formato: 0.2=20%): "))
-    periodo_incremento_egresos=int(input("- Periodo de incremento en meses: "))
+    #Comprobación
+    while incremento_egresos<0 or incremento_egresos>1: 
+        incremento_egresos=float(input("Error. Ingrese un valor decimal entre 0 y 1: "))
     
-    #Muestra de resultados
+    periodo_incremento_egresos=int(input("- Periodo de incremento en meses: "))
+    #Comprobación
+    while periodo_incremento_egresos<0:
+        periodo_incremento_egresos=int(input("Error. Ingrese un valor positivo: "))
+    
+    """--------------------Impresión de resultados-------------------------"""
+    
     print("\nPara ahorrar ", meta_ahorro, " MXN bajo los siguientes parámetros: ")
     print("\nIngresos actuales: ", balance, " MXN")
-    print("Egresos actuales: ", egresos_totales(egresos, costo_agua,\
-    costo_electricidad, costo_telefono_cable, costo_gas, costo_transporte,\
-    costo_extra), " MXN")
+    print("Egresos actuales: ", egresos, " MXN")
     print("Ahorrando el ", porcentaje_ahorro, " del balance")
     print("Los ingresos aumentan a razón de ",incremento_ingresos," cada ",\
     periodo_incremento_ingresos," mes/es")
@@ -193,12 +241,6 @@ elif opcion==3:
     balance,egresos, meta_ahorro, porcentaje_ahorro, incremento_ingresos,\
     periodo_incremento_ingresos, incremento_egresos,\
     periodo_incremento_egresos,meses), " mes/es")
-    
-    """ Todavía falta agregar los procesos de comprobación de los valores
-    ingresados para que indique cuando haya error y los cambie. Sobre esto,
-    tengo unas dudas que quiero aclarar primero antes de agregar los procesos.
-    Ya agendé una asesoría con el profe el lunes 27 de septiembre.
-    Para el siguiente avance, ya estarán los procesos de comprobación """
     
 else:
   print("Elija una opción válida")
